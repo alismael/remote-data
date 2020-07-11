bf96e39d7cae2581e7d939b2a4e6645435aece03
Ali Ismael <aliismaelabdelwahab@gmail.com>
2020-07-11 17:00:39 +0200

    Updates

---
+++ precache-manifest.21d49cbe3f27314f6810d8cd6ce10fc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e11b3cd1ad9ba15b88c8ec40229dfd1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7038bf91b71a8e29d81",
+    "url": "./static/css/main.f08bb530.chunk.css"
+  },
+  {
+    "revision": "7c0ca41075503818572c",
+    "url": "./static/js/2.1f7f829f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1f7f829f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7038bf91b71a8e29d81",
+    "url": "./static/js/main.b35ec581.chunk.js"
+  },
+  {
+    "revision": "a03c6724c80f2de879a6",
+    "url": "./static/js/runtime-main.96b0651b.js"
+  }
+]);
